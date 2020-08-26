558136f090a74daafc671a47dc9f7a66c1841188
oliviaraymond <50756091+oliviaraymond@users.noreply.github.com>
2020-08-26 15:43:44 -0600

    tier2

--- Revassess/src/sql/answer3.sql
+++ Revassess/src/sql/answer3.sql
@@ -1,1 +1,5 @@
-SELECT * FROM app_user WHERE user_id = '5';
+SELECT *  FROM app_user u
+JOIN study_set ss
+	ON u.user_id = ss.user_id 
+WHERE user_id = '5';
+
--- Revassess/src/sql/answer4.sql
+++ Revassess/src/sql/answer4.sql
@@ -1,1 +1,11 @@
-SELECT username,  categoryName, question, answer FROM app_user JOIN study_set ON user_id = owner_id JOIN study_set_card USING (study_set_id) JOIN flashcard USING (flashcard_id) JOIN category USING (category_id) WHERE user_id = (SELECT owner_id FROM study_set WHERE study_set_id = 4);
+SELECT u.username, f.categoryName, f.question, f.answer 
+FROM app_user u
+JOIN study_set ss
+	ON u.user_id = ss.user_id
+JOIN study_set_card 
+	USING (study_set_id) JOIN flashcard f
+		USING (flashcard_id) JOIN c category USING (category_id) 
+WHERE u.user_id = (SELECT user_id FROM study_set WHERE study_set_id = 4);
+
+
+

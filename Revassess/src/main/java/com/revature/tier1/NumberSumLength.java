d4134a0d61512c29e71bd8f12246199603d790fe
oliviaraymond <50756091+oliviaraymond@users.noreply.github.com>
2020-08-26 13:35:25 -0600

    tier 1 done

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,34 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+
+		// Create a method that, given an integer, can raise each digit to the power of
+		// the overall length and return whether the sum of the powers is equal to the
+		// original integer.
+		
+		return (getSum(num) == calculate(num));
+
+	}
+
+	static long calculate(long num) {
+		String numberString = Long.toString(num);
+
+		long sum = 0;
+		long bp = (int) Math.pow(num, numberString.length());
+		while (bp != 0) {
+			long d = bp % 10;
+			sum += d;
+			bp /= 10;
+		}
+		return sum;
+	}
+
+	static long getSum(long n) {
+		long sum = 0;
+		while (n != 0) {
+			sum = sum + n % 10;
+			n = n / 10;
+		}
+		return sum;
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,6 +3,10 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		int sum = 0;
+		for (int i : arr) {
+			sum = sum + i;
+		}
+		return sum;
 	}
 }
